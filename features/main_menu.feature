8c53e6d397fec0249e18d4e109f595122742465e
dericivd@gmail.com <vacipderici@gmail.com>
2022-03-12 21:52:30 +0300

    create first Cucumber scenario

---
+++ features/main_menu.feature
@@ -0,0 +1,5 @@
+Feature: As a user I want to convert units
+  Scenario:  When  I tab on menu icon , I should see left side menu
+    Given I land on home screen
+    When  I press on menu icon
+    Then  I should see left side menu
---
+++ features/step_definitions/main_menu_steps.rb
@@ -0,0 +1,9 @@
+Given('I land on home screen') do
+  puts("landed on home screen")
+end
+When('I press on menu icon') do
+  puts("menu icon pressed  ")
+end
+Then('I should see left side menu') do
+  puts("I see left side menu  ")
+end
